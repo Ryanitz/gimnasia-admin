e64694c6e859eb3509f577ca61aff58a70aa623e
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2022-03-10 17:26:08 -0300

    WIP

--- components/players.js
+++ components/players.js
@@ -1,7 +1,7 @@
 export default function Players({ players }) {
   return (
-    <div class="overflow-x-auto">
-      <table class="table w-full">
+    <div className="overflow-x-auto">
+      <table className="table w-full">
         <thead>
           <tr>
             <th></th>
